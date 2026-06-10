bb838f2e8d4bc5d5e90e74eee4e46586923561cf
Djii005 <suryaadjii17@gmail.com>
2026-06-10 14:26:07 +0800

    feat: connect project to supabase (postgresql)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@prisma/client', 'bcryptjs'],
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 module.exports = nextConfig;
+
